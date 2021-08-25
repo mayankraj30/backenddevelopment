93605330fb4d6563f8015d1700a944607417bea5
Mayank Raj <mayankrajaraj@gmail.com>
2021-08-25 19:05:54 +0530

    using cookies

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const url = 'mongodb://localhost:27017/confusion';
 
 const connect = mongoose.connect(url);
 
-connect.then((db) =>{
+connect.then((db) => {
   console.log("connected correctly to mongodb server");
-}, (err) => {console.log(err)});
+}, (err) => { console.log(err) });
 
 var app = express();
 
@@ -31,38 +31,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth(req,res,next){
-  console.log(req.headers);
+function auth(req, res, next) {
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
-  console.log(authHeader);
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    console.log(authHeader);
 
-  if(!authHeader){
-    var err = new Error('You are not authenticated');
+    if (!authHeader) {
+      var err = new Error('You are not authenticated');
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin',{ signed:true})
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated');
+
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+
   }
+  else{
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }
+    else{
+      var err = new Error('You are not authenticated');
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-
-  if (username === 'admin'  && password === 'password'){
-    next();
+      
+      err.status = 401;
+      return next(err);
+    }
   }
-  else {
-    var err = new Error('You are not authenticated');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);  
-  }
-
-
 }
 
 app.use(auth);
@@ -76,12 +91,12 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
